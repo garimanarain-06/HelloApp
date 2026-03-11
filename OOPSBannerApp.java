f8fb3a8198bd0d402a6f4086055a6efc799f9bbb
garimanarain-06 <garimanarain85@gmail.com>
2026-03-11 21:15:57 +0530

    Coded UC2 to Print OOPS Banner

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,18 +1,18 @@
 /**
- * Program Name : OOPSBannerApp
-<<<<<<< HEAD
- * Description  : Display OOPS in Banner Format
-=======
- * Description  : Prints OOPS to console
->>>>>>> feature/UC1-PrintOOPS
- * Author       : Garima Narain
- * Version      : 1.0
+ * OOPSBannerApp UC2 - Print OOPS Banner
  */
 
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
-        System.out.println("OOPS");
+
+        System.out.println(" *****    *****   ******    ***** ");
+        System.out.println("*     *  *     *  *     *  *     *");
+        System.out.println("*     *  *     *  *     *  *      ");
+        System.out.println("*     *  *     *  ******    ***** ");
+        System.out.println("*     *  *     *  *              *");
+        System.out.println("*     *  *     *  *        *     *");
+        System.out.println(" *****    *****   *         ***** ");
+
     }
 }
-
