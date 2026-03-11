b226d9561c602b1cdcfc1662063178be9d46bcc5
garimanarain-06 <garimanarain85@gmail.com>
2026-03-11 20:03:17 +0530

    UC2 OOPS Banner implementation

---
+++ OOPSBannerApp.java
@@ -0,0 +1,22 @@
+/**
+ * OOPSBannerApp UC2 – Banner Display Application
+ *
+ * This class displays the word "OOPS" in a large banner format
+ * using spaces and asterisks (*) with individual print statements.
+ *
+ * @author Gitika Jain
+ * @version 2.0
+ */
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+
+        System.out.println(" ***    ***   *****   ***** ");
+        System.out.println("*   *  *   *  *       *     ");
+        System.out.println("*   *  *   *  *****   ***** ");
+        System.out.println("*   *  *   *      *       * ");
+        System.out.println("*   *  *   *      *       * ");
+        System.out.println("*   *  *   *  *   *   *   * ");
+        System.out.println(" ***    ***    *****   ***** ");
+    }
+}
